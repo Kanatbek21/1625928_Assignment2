83de8007da1dc31194ab049e4c4a56b2e1136c70
Kanatbek21 <47849640+Kanatbek21@users.noreply.github.com>
2019-05-10 12:36:10 +0100

    Hotel_Test file
    Code for the Hotel Test

---
+++ Hotel_Test.java
@@ -0,0 +1,11 @@
+//Testing the Hotel_Management
+import javax.swing.JFrame;
+public class Hotel_Test {
+	public static void main(String[] args) {
+		// Creates a new JFrame for Hotel_Management class
+		Hotel_Management gridLayoutFrame = new Hotel_Management();
+		gridLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// Exit the application on close
+		gridLayoutFrame.setSize(1200, 800);									// Sets the size
+		gridLayoutFrame.setVisible(true);									// Sets the visibility
+	}
+}//end class Hotel_Testing
